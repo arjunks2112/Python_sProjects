e2a057549dc45a247930bf94650f2b1179cf73d6
Arjun K S <56349056+arjunks2112@users.noreply.github.com>
2021-08-26 21:20:13 +0530

    Add files via upload

---
+++ Text to speech/text_to_speech.py
@@ -0,0 +1,14 @@
+from gtts import gTTS
+
+import os
+
+f=open('test.txt')
+x=f.read()
+
+language="en"
+
+audio=gTTS(text=x,lang=language,slow=False)
+
+audio.save("test.wav")
+
+os.system("test.wav")
