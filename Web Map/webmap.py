a4aeea4392383106ac6e0ff9aa195318a534bb6f
Arjun K S <56349056+arjunks2112@users.noreply.github.com>
2021-09-18 21:55:51 +0530

    Update webmap.py

--- Web Map/webmap.py
+++ Web Map/webmap.py
@@ -4,6 +4,6 @@ fg = folium.FeatureGroup("map")
 fg.add_child(folium.GeoJson(date =(open("india_states.json" , "r" ,encoding="utf-8-sig").read())))
 
 
-map = folium.Map(location =(20.0000,75.0000),zoom_start = 4)
+map = folium.Map(location =(21.0000,75.0000),zoom_start = 4)
 map.add_child(fg)
 map.save("sample.html")
